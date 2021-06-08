d70fababb2b98de1c6d3f66ff83120bc8ac128dc
ClementRomac <clement.romac@gmail.com>
2021-06-08 16:03:56 +0200

    Fixed bug in download baselines script

--- notebooks/download_baselines.sh
+++ notebooks/download_baselines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+mkdir -p ../TeachMyAgent/data
 cd ../TeachMyAgent/data
 wget https://flowers.inria.fr/teachmyagent/baselines_results.zip --no-check-certificate
 unzip baselines_results.zip
-
